5627ab7219e7ecf2f4c1807c8b8c88b6aa35c187
Zunaed Ahmed <65343827+zun43d@users.noreply.github.com>
2023-11-26 18:24:33 +0000

    Discord deasbled

--- data/index.jsx
+++ data/index.jsx
@@ -1,4 +1,4 @@
 export const discord = 'https://discord.gg/4eFgkqSAuC'
-export const whitepaper = 'https://wrecked-worlds.gitbook.io/'
+export const whitepaper = 'https://wrecked-worlds.gitbook.io/2.0/'
 export const roadmap = 'https://wrecked-worlds.gitbook.io/whitepaper/road-map'
 export const twitter = 'https://twitter.com/wreckedworlds'
